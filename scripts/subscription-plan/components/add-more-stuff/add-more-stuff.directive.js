05f490fafa8c0ca74b8035d533ed01b852f2bcb3
svichv <Unlimitedagro11>
2019-09-13 17:03:50 +0300

    bux fix

--- scripts/subscription-plan/components/add-more-stuff/add-more-stuff.directive.js
+++ scripts/subscription-plan/components/add-more-stuff/add-more-stuff.directive.js
@@ -13,10 +13,12 @@
             link: function ($scope) {
                 var defaultPrice = $scope.subscriptionPlan.price;
 
-                $scope.discount = {
+                var defaulDiscountState = {
                     percentage: '0\%',
                     amount: '\$0'
                 };
+
+                $scope.discount = _.cloneDeep(defaulDiscountState);
 
                 $scope.isDiscountNotificationMessageShown = false;
 
@@ -25,10 +27,10 @@
                         discountAmount = 0;
 
                     stuffPrice += $scope.subscriptionPlan.adminCount > 1 ?
-                        $scope.subscriptionPlan.adminCount * SUBSCRIPTION_PLAN.stuffPrices.admin : 0;
+                        ($scope.subscriptionPlan.adminCount - 1) * SUBSCRIPTION_PLAN.stuffPrices.admin : 0;
 
                     stuffPrice += $scope.subscriptionPlan.techCount > 1 ?
-                        $scope.subscriptionPlan.techCount * SUBSCRIPTION_PLAN.stuffPrices.tech : 0;
+                        ($scope.subscriptionPlan.techCount - 1) * SUBSCRIPTION_PLAN.stuffPrices.tech : 0;
 
                     discountAmount =
                         (defaultPrice + stuffPrice) * SUBSCRIPTION_PLAN.discountPercentageByTechsCount[$scope.subscriptionPlan.techCount] / 100;
@@ -36,12 +38,17 @@
                     $scope.subscriptionPlan.price = (defaultPrice + stuffPrice - discountAmount).toFixed(2);
 
                     $scope.discount = {
-                        percentage:  SUBSCRIPTION_PLAN.discountPercentageByTechsCount[$scope.subscriptionPlan.techCount] + '\%',
+                        percentage: SUBSCRIPTION_PLAN.discountPercentageByTechsCount[$scope.subscriptionPlan.techCount] + '\%',
                         amount: '\$' + (discountAmount ? discountAmount.toFixed(2) : discountAmount)
                     };
 
                     $scope.isDiscountNotificationMessageShown = $scope.subscriptionPlan.techCount === 10;
                 };
+
+                $scope.$watch('subscriptionPlan', function () {
+                    $scope.discount = _.cloneDeep(defaulDiscountState);
+                    $scope.isDiscountNotificationMessageShown = false;
+                });
             }
         }
     })
